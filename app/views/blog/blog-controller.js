b1b07fc5b41d5619667696c8fc73eace4587bd99
Dylan Harness <dharness@uwo.ca>
2015-07-01 21:23:19 -0400

    ..

--- app/views/blog/blog-controller.js
+++ app/views/blog/blog-controller.js
@@ -10,11 +10,11 @@ angular.module('dharness-blog', [])
 
 		$scope.groups = [
 		{
-			title: 'Dynamic Group Header - 1',
+			title: 'Wrestling with Giants',
 			content: 'Dynamic Group Body - 1'
 		},
 		{
-			title: 'Dynamic Group Header - 2',
+			title: 'Philosophy',
 			content: 'Dynamic Group Body - 2'
 		}];
 
